a64326fe1f6530e72142ec4ced9ced7c0b05b61b
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-11-10 22:43:47 +0300

    GameHelper

---
+++ DotComGame/SimpleDotComGame.java
@@ -0,0 +1,25 @@
+package DotComGame;
+
+public class SimpleDotComGame {
+    public static void main(String[] args) {
+        int numOfGuesses = 0; // следит за кол-вом ходов пользователя
+        GameHelper helper = new GameHelper(); 
+    
+        SimpleDotCom theDotCom = new SimpleDotCom(); // создаем объект
+        int randomNum = (int) (Math.random() * 5); // генерация первой ячейки
+    
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2}; // формирование массива ячеек
+        theDotCom.setLocationCells(locations); // передаем местоположение ячеек
+        boolean isAlive = true; // для проверки, не закончилась ли игра
+    
+        while(isAlive == true) {
+            String guess = helper.getUserInput("Введите число"); // получаем строку от пользователя
+            String result = theDotCom.checkYourself(guess); // проверяет полученные данные
+            numOfGuesses++; // инкременируем кол-во попыток
+            if (result.equals("Потопил!")) {
+                isAlive = false;
+                System.out.println("Вам потребовалось " + numOfGuesses + " попыток(и)");
+            } // конец if
+        } // конец while
+    } // конец main
+}
