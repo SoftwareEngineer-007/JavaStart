acfce2fb13bf0fa2a8e6d58268793bf6f703257f
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-11-12 01:51:50 +0300

    removing unnecessary code

--- DotComGame/DotComBust.java
+++ DotComGame/DotComBust.java
@@ -73,30 +73,3 @@ public class DotComBust {
         game.startPlaying(); // начать главный игровой цикл
     } // конец метода
 }
-
-
-/* 
-public class DotComBust {
-    public static void main(String[] args) {
-        int numOfGuesses = 0; // следит за кол-вом ходов пользователя
-        GameHelper helper = new GameHelper(); 
-    
-        DotCom theDotCom = new DotCom(); // создаем объект
-        int randomNum = (int) (Math.random() * 5); // генерация первой ячейки
-    
-        ArrayList<String> locations = {randomNum, randomNum + 1, randomNum + 2}; // формирование массива ячеек
-        theDotCom.setLocationCells(locations); // передаем местоположение ячеек
-        boolean isAlive = true; // для проверки, не закончилась ли игра
-    
-        while(isAlive == true) {
-            String guess = helper.getUserInput("Введите число"); // получаем строку от пользователя
-            String result = theDotCom.checkYourself(guess); // проверяет полученные данные
-            numOfGuesses++; // инкременируем кол-во попыток
-            if (result.equals("Потопил!")) {
-                isAlive = false;
-                System.out.println("Вам потребовалось " + numOfGuesses + " попыток(и)");
-            } // конец if
-        } // конец while
-    } // конец main
-}
-*/
