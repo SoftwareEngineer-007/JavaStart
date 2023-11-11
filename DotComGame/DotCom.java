7c706dd574f3823893147b13be6c0266cbd9181c
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-11-12 01:50:45 +0300

    final version

--- DotComGame/DotCom.java
+++ DotComGame/DotCom.java
@@ -1,14 +1,18 @@
 package DotComGame;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class DotCom {
     
-    private ArrayList<String> locationCells;
-    // private int numOfHits;
+    private ArrayList<String> locationCells; //ArrayList с ячейками описывающими местоположение
+    private String name; // имя "сайта"
 
-    public void setLocationCells(ArrayList<String> loc) {
+    public void setLocationCells(ArrayList<String> loc) { // сеттер обновляющий местоположение "сайта"
         locationCells = loc;
+    }
+
+    public void setName(String n) { // простой сеттер
+        name = n;
     }
 
     public String checkYourself(String userInput) {
@@ -16,14 +20,15 @@ public class DotCom {
 
     int index = locationCells.indexOf(userInput);
     if (index >= 0) {
-        locationCells.remove(index);
+        locationCells.remove(index); // применяем метод remove из ArrayList для удаления элемента
 
-        if (locationCells.isEmpty()) {
+        if (locationCells.isEmpty()) { // проверка, все ли адреса были разгаданы
             result = "Потопил!!!";
+            System.out.println("Ой, вы потопили " + name + " :("); // сообщение пользователю о потоплении
         } else {
             result = "Попал. Так держать";
         } // конец if
     } // конец внешнего if
-    return result;
+    return result; // возвращаем результат
     } // конец метода
 } // конец класса
--- DotComGame/DotComBust.java
+++ DotComGame/DotComBust.java
@@ -2,68 +2,80 @@ package DotComGame;
 
 import java.util.*;
 
-private GameHelper helper = new GameHelper();
-private ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
-private int numOfGuesses = 0;
+public class DotComBust {
+// объявляем и инициализируем переменные, которые нам понадобятся
+    private GameHelper helper = new GameHelper();
+    private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>(); // ArrayList только для DotCom
+    private int numOfGuesses = 0;
 
-private void setUpGame() {
-    // Создадим несколько "сайтов" и присвоим им адреса
-    DotCom one = new DotCom();
-    one.setName("Pets.com");
-    DotCom two = new DotCom();
-    two.setName("eToys.com");
-    DotCom three = new DotCom();
-    three.setName("Go2.com");
-    dotComsList.add(one);
-    dotComsList.add(two);
-    dotComsList.add(three);
+    private void setUpGame() {
+        // Создадим несколько "сайтов" и присвоим им адреса
+        DotCom one = new DotCom();
+        one.setName("Pets.com");
+        DotCom two = new DotCom();
+        two.setName("eToys.com");
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+        dotComsList.add(one);
+        dotComsList.add(two);
+        dotComsList.add(three);
+// краткие инструкции для пользователя
+        System.out.println("Ваша цель - потопить три 'сайта'.");
+        System.out.println("Pets.com, eToys.com, Go2.com");
+        System.out.println("Попытайтесь потопить их за минимальное количество ходов");
 
-    System,out.println("Ваша цель - потопить три 'сайта'.");
-    System,out.println("Pets.com, eToys.com, Go2.com");
-    System,out.println("Попытайтесь потопить их за минимальное количество ходов");
+        for (DotCom dotComToSet : dotComsList) {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+        } // конец цикла
+    } //конец метода setUpGame
 
-    for (DotCom dotComToSet : dotComsList) {
-        ArrayList<String> newLocation = helper.placeDotCom(3);
-        dotComToSet.setLocationCells(newLocation);
-    } // конец цикла
-} //конец метода setUpGame
+    private void startPlaying() {
+        while (!dotComsList.isEmpty()) { // пока список DotCom не станет пустым
+            String userGuess = helper.getUserInput("Сделайте ход"); // получаем пользовательский ввод
+            checkUserGuess(userGuess); // вызываем метод checkUserGuess
+        } // конец while
+        finishGame(); // вызываем метод 
+    } // конец метода startPlaying method
 
-private void startPlaying() {
-    while (!dotComList.isEmpty()) {
-        String userGuess = helper.getUserInput("Сделайте ход");
-        checkUserGuess(userGuess);
-    } // конец while
-    finishGame();
-} // конец метода startPlaying method
+    private void checkUserGuess(String userGuess) {
+        numOfGuesses++; // никрементируем кол-во попыток, которые сдклал пользователь
+        String result = "Мимо:("; // подразумеваем промах, пока не выяснили обратного
 
-private void checkUserGuess(String userGuess) {
-    numOfGuesses++;
-    String result = "Мимо:(";
+        for (DotCom dotComToTest : dotComsList) { // повторяем для всех объектов DotCom в списке
+            result = dotComToTest.checkYourself(userGuess); // DotCom проверяет ход пользователя
+            if (result.equals("Попал!")) {
+                break; // выбираемся из цикла раньше времени
+            }
+            if (result.equals("Потопил!!!")) {
+                dotComsList.remove(dotComToTest); // удаляем из списка "сайтов" и выходим из цикла
+                break;
+            }
+        } // конец for
+        System.out.println(result); // выводим результат
+    } // конец метода
 
-    for (DotCom dotComToTest : dotComList) {
-        result = dotComToTest.checkYourself(userGuess);
-        if (result.equals("Попал!")) {
-            break;
+    private void finishGame() { 
+// выводим сообщение для пользователя, как он прошел игру
+        System.out.println("Все 'сайты' ушли ко дну! Хорошая работа!");
+        if (numOfGuesses <= 18) {
+            System.out.println("Это заняло у вас всего " + numOfGuesses + " попыток.");
+            System.out.println("Вы успели до того, как по вам открыли ответный огонь.");
+        } else {
+            System.out.println("Это заняло у вас довольно много времени. " + numOfGuesses + " попыток.");
+            System.out.println("Рыбы водят хороводы вокруг вас))");
         }
-        if (result.equals("Потопил!!!")) {
-            dotComList.remove(dotComToTest);
-            break;
-        }
-    } // конец for
-    System.out.println(result);
-} // конец метода
+    } // конец метода
 
-private void finishGame() {
-    System.out.println("Все 'сайты' ушли ко дну! Хорошая работа!");
-    if (numOfGuesses <= 18) {
-        System,out.println("Это заняло у вас всего " + numOfGuesses + " попыток.");
-        System,out.println("Вы успели до того, как по вам открыли ответный огонь.");
-    } else {
-        System,out.println("Это заняло у вас довольно много времени. " + numOfGuesses + " попыток.");
-        System,out.println("Рыбы водят хороводы вокруг вас))");
-    }
-} // конец метода
+    public static void main(String[] args) {
+        DotComBust game = new DotComBust(); // создаем игровой объект
+        game.setUpGame(); // подготовить игру
+        game.startPlaying(); // начать главный игровой цикл
+    } // конец метода
+}
 
+
+/* 
 public class DotComBust {
     public static void main(String[] args) {
         int numOfGuesses = 0; // следит за кол-вом ходов пользователя
@@ -87,3 +99,4 @@ public class DotComBust {
         } // конец while
     } // конец main
 }
+*/
