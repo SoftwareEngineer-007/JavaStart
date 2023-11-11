f5194b614084c61e941d706b48206405df6cda65
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-11-11 22:47:48 +0300

    fix errors

--- DotComGame/SimpleDotCom.java
+++ DotComGame/SimpleDotCom.java
@@ -1,28 +1,29 @@
 package DotComGame;
 
+import java.util.ArrayList;
+
 public class SimpleDotCom {
     
-    int[] locationCells;
-    int numOfHits = 0;
-    public void setLocationCells(int[] locs) {
-        locationCells = locs;
+    private ArrayList<String> locationCells;
+    // private int numOfHits;
+
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
     }
 
-    public String checkYourself(String stringGuess) {
-    int guess = Integer.parseInt(stringGuess);
+    public String checkYourself(String userInput) {
     String result = "Мимо";
-    for (int cell : locationCells) {
-        if  (guess == cell) {
-            result = "Попал!";
-            numOfHits++;
-            break;
-        } //конец if
-    } //выход из цикла
 
-    if (numOfHits == locationCells.length) {
-        result = "Потопил!";
-    }
-    System.out.println(result);
-        return result;
-    }
-}
+    int index = locationCells.indexOf(userInput);
+    if (index >= 0) {
+        locationCells.remove(index);
+
+        if (locationCells.isEmpty()) {
+            result = "Потопил!!!";
+        } else {
+            result = "Попал. Так держать";
+        } // конец if
+    } // конец внешнего if
+    return result;
+    } // конец метода
+} // конец класса
